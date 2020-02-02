04ab2d6d9b3f9f9969acf53c4eeb487c292e1c24
tkc1009 <tkc1009@users.noreply.github.com>
2020-02-02 12:03:57 +0900

    modify WaterLevelManager related to the time

--- Assets/Scripts/WaterLevelManager.cs
+++ Assets/Scripts/WaterLevelManager.cs
@@ -2,28 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class WaterLevelManager : MonoBehaviour
 {
-
     Text waterLevelText;
     float waterLevel = 0;
-    float drownSpeed = 0f;
-
+    float speed;
+    int cracks;
+    float drownSpeed;
     // Start is called before the first frame update
     void Start()
     {
+        waterLevel = 0;
+        speed = Time.deltaTime;
+        cracks = 0;
+        drownSpeed = 0f;
         waterLevelText = GetComponent<Text>();
     }
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.U))
         {
-            drownSpeed += Time.deltaTime;
-            waterLevel += drownSpeed * 100;
-            waterLevelText.text = ((int)waterLevel).ToString("000");
+            cracks += 1;
         }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (cracks > 0)
+            {
+                cracks -= 1;
+            }
+        }
+        drownSpeed = speed * cracks;
+        waterLevel += drownSpeed * 10;
+        waterLevelText.text = ((int)waterLevel).ToString("000");
     }
 }
