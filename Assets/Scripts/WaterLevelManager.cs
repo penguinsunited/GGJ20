9f7fc03d8443a88c033e477cede18ce39941b98a
tkc1009 <tkc1009@users.noreply.github.com>
2020-02-02 11:05:48 +0900

    add only WaterLevelManager script

---
+++ Assets/Scripts/WaterLevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaterLevelManager : MonoBehaviour
+{
+
+    Text waterLevelText;
+    float waterLevel = 0;
+    float drownSpeed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waterLevelText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            drownSpeed += Time.deltaTime;
+            waterLevel += drownSpeed * 100;
+            waterLevelText.text = ((int)waterLevel).ToString("000");
+        }
+    }
+}
